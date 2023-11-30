ab774956a40c71ecbe6af4a7001d1c23692da1be
Alexander Belov <alexander.belov@zyfra.com>
2023-11-30 20:43:45 +0300

    add variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,8 +12,14 @@ variable "yc_folder_id" {
 
 variable "yc_zones" {
   type = list(string)
+  default = ["ru-central1-a", "ru-central1-b", "ru-central1-c"]
 }
 
 variable "yc_subnets" {
   type = list(list(string))
+  default = [
+      ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"], 
+      ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"], 
+      ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+    ]
 }
